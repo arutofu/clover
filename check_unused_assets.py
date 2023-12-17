170434de1593941da03fbb525d6c1faf47427b11
arutofu <Arutofu@yandex.ru>
2023-12-17 16:12:38 +1100

    Update book

--- check_unused_assets.py
+++ check_unused_assets.py
@@ -16,7 +16,7 @@ for root, dirs, files in os.walk('assets'):
         if f not in EXCLUDE:
             path = os.path.join(root, f)
             try:
-                subprocess.check_output(['grep', '-F', '-r', path, './ru', './en'])
+                subprocess.check_output(['grep', '-F', '-r', path, './ru'])
             except subprocess.CalledProcessError:
                 print('\x1b[1;31mAssets file {} is not used\x1b[0m'.format(path))
                 code = 1
