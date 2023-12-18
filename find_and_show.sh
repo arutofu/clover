bcbbfb22ffaee0b4b65fcc50d4b0357608d0a4de
arutofu <Arutofu@yandex.ru>
2023-12-18 19:25:14 +1100

    Update find_and_show.sh

--- find_and_show.sh
+++ find_and_show.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
-target_string='<a class="btn pull-right js-toolbar-action" aria-label="" href="#"><i class="fa fa-twitter"></i></a>'
-files_with_string=$(grep -rl "$target_string" _book)
+gitbook_file="_book/gitbook"
+
+if [ -f "$gitbook_file" ]; then
+  echo "Contents of $gitbook_file:"
+  cat "$gitbook_file"
+else
+  echo "File $gitbook_file not found."
+fi
+
+target_string='twitter'
+files_with_string=$(grep -rl "$target_string" _book/*)
 echo "Files containing the target string:"
 echo "$files_with_string"
 
-# Output content of files
 for file in $files_with_string; do
   echo "Content of $file:"
   cat "$file"
-done || true  # Продолжить выполнение даже если cat завершится неудачно (для пустых файлов)
+done || true
+
+ru_folder="_book/ru"
+if [ -d "$ru_folder" ]; then
+  echo "Contents of $ru_folder:"
+  ls -l "$ru_folder"
+else
+  echo "Directory $ru_folder not found."
+fi
