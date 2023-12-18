9774c162ebfae07cadd81b525712f82817251a99
arutofu <Arutofu@yandex.ru>
2023-12-18 20:02:35 +1100

    Update find_and_show.sh

--- find_and_show.sh
+++ find_and_show.sh
@@ -23,3 +23,15 @@ else
 fi
 
 echo "--------------------------------------------------------"
+
+buttons_file="_book/gitbook/gitbook-plugin-sharing/buttons.js"
+
+# Проверяем наличие файла buttons.js и выводим его содержимое
+if [ -f "$buttons_file" ]; then
+  echo "Contents of $buttons_file:"
+  cat "$buttons_file"
+else
+  echo "File $buttons_file not found."
+fi
+
+echo "--------------------------------------------------------"
