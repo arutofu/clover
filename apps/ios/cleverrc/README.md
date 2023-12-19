9a234b207681c41ece7940ac37ca2ba423e3a965
arutofu <Arutofu@yandex.ru>
2023-12-19 21:03:49 +1100

    Update folders

--- apps/ios/cleverrc/README.md
+++
@@ -1,9 +0,0 @@
-# iOS-приложение
-
-Для установки зависимостей необходим [CocoaPods](https://cocoapods.org):
-
-```bash
-pod install
-```
-
-Для разработки и сборки откройте в XCode файл `dronerrc.xcworkspace`.
--- builder/image-ros.sh
+++ builder/image-ros.sh
@@ -153,7 +153,7 @@ fi
 
 # replace assets copy to assets symlink to save space
 rm -rf _book/assets && ln -s ../docs/assets _book/assets
-touch node_modules/CATKIN_IGNORE docs/CATKIN_IGNORE _book/CATKIN_IGNORE drone/www/CATKIN_IGNORE apps/CATKIN_IGNORE # ignore documentation files by catkin
+touch node_modules/CATKIN_IGNORE docs/CATKIN_IGNORE _book/CATKIN_IGNORE drone/www/CATKIN_IGNORE # ignore documentation files by catkin
 
 echo_stamp "Installing additional ROS packages"
 my_travis_retry apt-get install -y --no-install-recommends \
