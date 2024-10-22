2b4afed91e39a00c84d7d27ff2f5a1fb12affd8b
arutofu <Arutofu@yandex.ru>
2024-10-23 02:01:32 +1100

    Update image-sensors.sh

--- builder/image-sensors.sh
+++ builder/image-sensors.sh
@@ -33,14 +33,18 @@ sudo apt-get install -y python3 python3-pip
 
 # Устанавливаем библиотеку для работы с DHT11 (датчик температуры и влажности)
 sudo pip3 install Adafruit_DHT
+echo "Adafruit_DHT for DHT11 installed"
 
 # Устанавливаем библиотеки для работы с GPIO
 sudo pip3 install RPi.GPIO
+echo "RPi.GPIO for GPIO installed"
 
 sudo apt-get install -y wget unzip git
+echo "wget installed"
 
 # Устанавливаем библиотеку wiringPi
 sudo apt-get install -y wiringpi
+echo "wiringPi installed"
 
 # Устанавливаем библиотеку для работы с RTC DS1302 (модуль часов реального времени)
 git clone https://github.com/dimp-gh/rpi-ds1302.git
@@ -48,9 +52,11 @@ cd rpi-ds1302
 sudo python3 setup.py install
 cd ..
 rm -rf rpi-ds1302
-
-# Для остальных датчиков не требуется установка дополнительных библиотек, так как они используют стандартные GPIO библиотеки
+echo "rpi-ds1302 for RTC DS1302 installed"
 
 # Устанавливаем дополнительную библиотеку для работы с I2C, если понадобится
 sudo apt-get install -y python3-smbus python3-dev i2c-tools
+echo "Installing the library for I2C just in case"
 
+# Для остальных датчиков не требуется установка дополнительных библиотек, так как они используют стандартные GPIO библиотеки
+echo "Others sensors dont want any libraries"
